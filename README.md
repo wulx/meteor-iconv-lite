5972b6d214eeb712302ec464c62ecd0cc3717a18
Gurdy Woo <gurdy.woo@gmail.com>
2013-08-22 15:01:05 +0800

    npm integration revised

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# iconv-lite repackaged for meteor
+# iconv-lite
+## npm integration for meteor
 [iconv-lite](https://github.com/ashtuchkin/iconv-lite) - pure javascript character encoding conversion
--- package.js
+++ package.js
@@ -1,15 +1,16 @@
 Package.describe({
-  summary: "iconv-lite - pure javascript character encoding conversion (repackaged for meteor)"
+  summary: "iconv-lite - pure javascript character encoding conversion (npm integrated for meteor)"
 });
 
 Npm.depends({
   // "vows": "0.7.0", // for test only
-  "iconv": "2.0.5"
+  "iconv": "2.0.5",
+  "iconv-lite": "0.2.11"
 });
 
 Package.on_use(function (api) {
-  api.export('iconv', 'server');
   api.add_files('iconv-lite.js', 'server');
+  api.export('iconv', 'server');
 
   if (typeof api.export !== 'undefined') {
     api.use('webapp', 'server');
